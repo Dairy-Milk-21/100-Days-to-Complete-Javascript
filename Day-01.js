ed6d6f76a5158a13bfa13b264cd3dedc34be0f5f
Abrar Daiyan <abradabra212009@gmail.com>
2025-03-14 16:37:29 +0600

    js

---
+++ Day-01.js
@@ -0,0 +1,29 @@
+// There are four types of values existing in js
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+
+// Also there are 4 data types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+
+// To print out function console.log() is used
+console.log(5); // Number
+console.log("Hello World"); // String
+console.log(true); // Boolean
+console.log(undefined); // Undefined
+
+// To declare a variable let keyword is used
+let x = 5;
+console.log(x);
+
+// To declare a constant const keyword is used
+const y = 10;
+console.log(y);
+
+// To declare a variable var keyword is used too
+var z = 15;
+console.log(z);
